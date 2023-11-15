0891a99e20039ce34096778386df20f365a79ed5
Milene01 <cavalcantemilene01@alu.ufc.br>
2023-11-15 17:57:52 -0300

    Removendo o campo de Senha em Alterar Perfil

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -33,7 +33,6 @@ module.exports = {
         validations: [
             body('name').isLength({ min: 3, max: 100 }).withMessage("O nome deve ter entre 3 e 100 caracteres").not().isEmpty().withMessage("Preencha o campo nome").isAlpha("pt-BR", {ignore:" "}).withMessage("O nome não deve conter caracteres especiais"),
             body('email').isLength({ min: 3, max: 100 }).withMessage("O email deve ter entre 3 e 100 caracteres").isEmail().withMessage("O campo deve ser um email válido").not().isEmpty().withMessage("Preencha o campo email"),
-            body('password').isLength({ min: 8 }).withMessage("A senha deve ter no mínimo 8 caracteres").not().isEmpty().withMessage("Preencha o campo senha"),
             body('birthday').isDate({ format: 'YYYY-MM-DD', strictMode: true }).withMessage("A data de nascimento deve ser uma data válida"),
             body('gender').isInt({ min: 1, max: 3 }).withMessage("O campo gênero é inválido"),
             body('company').isLength({ max: 100 }).withMessage("A empresa deve ter no máximo 100 caracteres"),
@@ -50,7 +49,7 @@ module.exports = {
 
                 const id = req.user_id;
 
-                const { name, email, password, birthday, gender, company, role, avatar } = req.body;
+                const { name, email, birthday, gender, company, role, avatar } = req.body;
 
                 const user = await User.findOne({
                     where: {id},
@@ -66,7 +65,35 @@ module.exports = {
                     }
                 }
 
-                const updated_user = await User.update({ name, email, password: bcrypt.hashSync(password, 8), birthday, gender, company, role, avatar }, {
+                const updated_user = await User.update({ name, email, birthday, gender, company, role, avatar }, {
+                    where: { id }
+                });
+
+                return res.status(200).send();
+
+            } catch (error) {
+                return handleExceptions(error, res);
+            }
+        }
+    },
+
+    changePassword: {
+        validations: [
+            body('password').isLength({ min: 8 }).withMessage("A senha deve ter no mínimo 8 caracteres").not().isEmpty().withMessage("Preencha o campo senha")
+        ], 
+        handler: async (req, res) => {
+            
+            try {
+
+                const errors = validationResult(req);
+                if (!errors.isEmpty()) 
+                    throw {name: 'RequestValidationError', errors};
+
+                const id = req.user_id;
+
+                const {password} = req.body;
+
+                const user = await User.update({password: bcrypt.hashSync(password, 8)}, {
                     where: { id }
                 });
 
