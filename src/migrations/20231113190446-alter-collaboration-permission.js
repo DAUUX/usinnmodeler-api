bea24252d6530e191b46dafdcf595336b2d329ed
caio <caiof557@gmail.com>
2023-12-02 15:33:29 -0300

    correcao do erro que nao exibia a opcao de compartilhar diagrama

---
+++ src/migrations/20231113190446-alter-collaboration-permission.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('collaboration', 'permission', {
+      type: Sequelize.INTEGER(1),
+      after: "diagram_id",
+      comment: "1:Leitor | 2:Editor "
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('collaboration', 'permission');
+  }
+};
