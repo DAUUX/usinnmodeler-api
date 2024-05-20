00bc9c3b4f61f84d819a0d78777a932bde1d8754
caio <caiof557@gmail.com>
2024-05-20 16:10:32 -0300

    alterado nome login para e-mail

--- src/controllers/AuthenticationController.js
+++ src/controllers/AuthenticationController.js
@@ -63,13 +63,13 @@ module.exports = {
                 });
 
                 if (!user)
-                    return res.status(404).json({ errors: [{ msg: "Login ou senha inválidos"}] });
+                    return res.status(404).json({ errors: [{ msg: "E-mail ou senha inválidos"}] });
 
                 // check credentials
                 let passwordIsValid = bcrypt.compareSync(password, user.password);
 
                 if (!passwordIsValid)
-                    return res.status(401).json({ errors: [{ msg: "Login ou senha inválidos" }] });
+                    return res.status(401).json({ errors: [{ msg: "E-mail ou senha inválidos" }] });
 
                 // creating access token
                 let token = jwt.sign({ id: user.id }, auth.secret, {
